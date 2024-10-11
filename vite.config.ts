0b3c3e1e894d9d1661b25515c9381d1da79b64da
Andrii <andrii.kulchytskyi@xorux.com>
2024-10-11 11:14:34 +0200

    add:initiate app & biomejs+pwa+react+ts <Star Wars Card Game>

---
+++ vite.config.ts
@@ -0,0 +1,39 @@
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+	plugins: [
+		react(),
+		VitePWA({
+			registerType: 'autoUpdate',
+			injectRegister: false,
+
+			pwaAssets: {
+				disabled: false,
+				config: true,
+			},
+
+			manifest: {
+				name: 'Star Wars Stone Card Game',
+				short_name: 'Star Wars Stone Card Game',
+				description: 'Star Wars Stone Card Game',
+				theme_color: '#ffffff',
+			},
+
+			workbox: {
+				globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
+				cleanupOutdatedCaches: true,
+				clientsClaim: true,
+			},
+
+			devOptions: {
+				enabled: false,
+				navigateFallback: 'index.html',
+				suppressWarnings: true,
+				type: 'module',
+			},
+		}),
+	],
+})
