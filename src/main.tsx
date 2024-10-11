8f349d8d075c60ae172a4c40ab75186aba80b110
Andrii <andrii.kulchytskyi@xorux.com>
2024-10-11 15:53:59 +0200

    add: basic settings <Star Wars Card Game>

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import Home from './pages/home/Home.tsx'
 import './index.css'
-import PWABadge from './PWABadge.tsx'
+import PWABadge from './types/ui/pwa-badge/PWABadge.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
-		<App />
+		<Home />
 		<PWABadge />
 	</React.StrictMode>
 )
